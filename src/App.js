e8c36e6463f35cc8f0bf186d3d1c82391efbb86e
Morning <morning@admins-iMac.local>
2023-01-06 12:35:32 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Nav from './Nav/Nav.js'
+
+function App() {
+  return (
+    <div className="App">
+        <Nav/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Nav/Nav.js
@@ -0,0 +1,22 @@
+import './NavStyle/NavStyle.css'
+
+export default function nav() {
+    return (
+            <div className="nav-main">
+                <div className="nav-left flex">
+                    <div className='navImage'>
+                    <img src="./Nav.svg" alt="" />
+                </div>
+                <div className='allsearch'>
+                    <input className="search" placeholder='Serach any things'></input>
+                    <button className='searchBtn'>Search</button>
+                </div>
+                </div>
+                <div className="nav-right flex">
+                    <img src="../user.svg" alt="" />
+                    <div className='Signin'><p>Sign in</p></div>
+                    <img src="../shopping.svg" alt="" className='shopping' />
+                </div>
+            </div>
+    )
+}
